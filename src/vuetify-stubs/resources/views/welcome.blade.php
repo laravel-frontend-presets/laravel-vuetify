f89dbc425b133fc34d13d2705caac4cfd5cbe5ec
Josh <4635851+KronemeyerJoshua@users.noreply.github.com>
2019-12-09 14:55:10 -0700

    Update welcome.blade.php

--- src/vuetify-stubs/resources/views/welcome.blade.php
+++ src/vuetify-stubs/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
-    <link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
         [v-cloak] > * { display:none; }
         [v-cloak]::before {
